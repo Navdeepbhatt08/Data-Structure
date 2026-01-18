a97596f48f93dd42d97df6d73267869ca5ea4a5a
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-18 14:16:12 +0530

    final

--- findDistinct.c
+++ findDistinct.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-// Main function
 int main()
 {
-    int arr1[100], n, ctr = 0;  // Declare an array to store integer values, n for array size, and ctr for counting duplicates
-    int i, j, k;  // Declare loop counters
- 
-    
+    int arr1[100], n, ctr = 0;
+    int i, j, k;
+
     printf("Input %d elements in the array :\n", n);
     for (i = 0; i < n; i++)
     {
         printf("element - %d : ", i);
-        scanf("%d", &arr1[i]);  // Read the input and store it in the array
+        scanf("%d", &arr1[i]);
     }
-    // Print unique elements in the array
+
     printf("\nThe unique elements found in the array are: \n");
     for (i = 0; i < n; i++)
     {
-        ctr = 0;  // Reset the counter for each element
+        ctr = 0;
         for (j = 0, k = n; j < k + 1; j++)
         {
-            /* Increment the counter when the search value is duplicate. */
+
             if (i != j)
             {
                 if (arr1[i] == arr1[j])
@@ -30,9 +28,9 @@ int main()
         }
         if (ctr == 0)
         {
-            printf("%d ", arr1[i]);  // Print the unique element
+            printf("%d ", arr1[i]);
         }
     }
     printf("\n\n");
-    return 0;  // Return 0 to indicate successful execution
+    return 0;
 }
