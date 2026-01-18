d8b2d8b45b473e8a71f649fbf8c0813c10d05704
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-18 14:11:44 +0530

    Distinct

--- findDistinct.c
+++ findDistinct.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
-void distinctElements(int arr[], int size) {
+void distinctElements(int arr[], int size)
+{
     printf("Distinct elements in the array are: ");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         int isDistinct = 1;
-        // Check if the current element has any duplicate before it
-        for (int j = 0; j < i; j++) {
-            if (arr[i] == arr[j]) {
-                isDistinct = 0; // It's a duplicate, not distinct
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                isDistinct = 0;
                 break;
             }
         }
-        if (isDistinct) {
+        if (isDistinct)
+        {
             printf("%d ", arr[i]);
         }
     }
     printf("\n");
 }
 
-int main() {
+int main()
+{
     int arr[] = {1, 4, 6, 1, 2, 8, 4, 7, 6, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
     printDistinctElements(arr, n);
