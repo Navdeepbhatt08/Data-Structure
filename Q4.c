18d9260295bd04b074955d3a1a6c916496139eff
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-20 19:17:06 +0530

    Saved

---
+++ Q4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, j, count;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Frequency of each element:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == -1)   // already counted
+            continue;
+
+        count = 1;
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] == a[j])
+            {
+                count++;
+                a[j] = -1;   // mark as counted
+            }
+        }
+
+        printf("%d occurs %d times\n", a[i], count);
+    }
+
+    return 0;
+}
