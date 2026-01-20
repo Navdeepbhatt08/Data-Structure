afb9ddc6b248c9cfe4ad16b3fa811f229d365ae3
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-20 19:33:02 +0530

    Saved

---
+++ Q6.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+    int d, missing = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    /* Find minimum difference */
+    d = a[1] - a[0];
+    for(i = 2; i < n; i++)
+    {
+        if(a[i] - a[i - 1] < d)
+            d = a[i] - a[i - 1];
+    }
+
+    /* Find missing term */
+    for(i = 1; i < n; i++)
+    {
+        if(a[i] - a[i - 1] != d)
+        {
+            missing = a[i - 1] + d;
+            break;
+        }
+    }
+
+    if(missing != 0)
+    {
+        printf("Missing term is: %d\n", missing);
+        printf("AP series including missing term:\n");
+
+        for(i = 0; i < n; i++)
+        {
+            printf("%d ", a[i]);
+            if(a[i] + d == missing)
+                printf("%d ", missing);
+        }
+    }
+    else
+    {
+        printf("The given sequence is a complete AP (no missing term)");
+    }
+
+    return 0;
+}
