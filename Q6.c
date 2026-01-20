ce39d7be2172e67d45e965d58e31b8eab5eeb6a1
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-20 19:33:50 +0530

    Commit Saved

--- Q6.c
+++ Q6.c
@@ -9,35 +9,35 @@ int main()
     scanf("%d", &n);
 
     printf("Enter %d elements:\n", n);
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
     d = a[1] - a[0];
-    for(i = 2; i < n; i++)
+    for (i = 2; i < n; i++)
     {
-        if(a[i] - a[i - 1] < d)
+        if (a[i] - a[i - 1] < d)
             d = a[i] - a[i - 1];
     }
 
-    for(i = 1; i < n; i++)
+    for (i = 1; i < n; i++)
     {
-        if(a[i] - a[i - 1] != d)
+        if (a[i] - a[i - 1] != d)
         {
             missing = a[i - 1] + d;
             break;
         }
     }
 
-    if(missing != 0)
+    if (missing != 0)
     {
         printf("Missing term is: %d\n", missing);
         printf("AP series including missing term:\n");
 
-        for(i = 0; i < n; i++)
+        for (i = 0; i < n; i++)
         {
             printf("%d ", a[i]);
-            if(a[i] + d == missing)
+            if (a[i] + d == missing)
                 printf("%d ", missing);
         }
     }
@@ -45,6 +45,5 @@ int main()
     {
         printf("The given sequence is a complete AP (no missing term)");
     }
-
     return 0;
 }
