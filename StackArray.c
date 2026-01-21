eefc232332825d6898fa91df6995e9f288529174
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-21 17:46:07 +0530

    Push Operation

--- StackArray.c
+++ StackArray.c
@@ -1,8 +1,12 @@
 #define MAX 10
 #include<stdio.h>
+
 int push(int[],int);
+
 int main()
 {
-  
-
+   int stack[MAX],top = -1;
+   char choise;
+     printf("Enter the Number you want to push : ");
+     scanf("%d ",&stack);
 }
