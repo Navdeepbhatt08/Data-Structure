7e62df0f60911d53650b46fdb87359a871466972
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-24 17:08:42 +0530

    cOMMIT

--- QueueOperations.c
+++ QueueOperations.c
@@ -1,7 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MAX 10
 
-int enqueue()
+int enqueue(int[], int, int);
+int dequeue(int[], int, int);
+int display(int[], int, int);
+
+main()
 {
-    
+    int queue[MAX], front = -1, rear, ch;
+
+    do
+    {
+        printf("\n 1. Enqueue \n 2. Dequeue \n 3. Display \n Enter your choice : ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            front = enqueue(queue, front, rear);
+            break;
+        case 2:
+            rear = enqueue(queue, front, rear);
+            break;
+        case 3:
+            front = enqueue(queue, front, rear);
+            break;
+        }
+    } while (ch <= 3);
 }
