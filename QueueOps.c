bebd90c67176a6a762f02d817455f8685d357158
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-25 16:57:10 +0530

    Saved

--- QueueOps.c
+++ QueueOps.c
@@ -8,41 +8,45 @@ void display(int[], int, int);
 main()
 {
 
-int queue[size],front = 0,rear = -1 ,ch;
-do
-{
-    printf("\n 1. Enqueue \n 2. DeQueue \n 3. Display \n Enter your choice : ");
-    scanf("%d",&ch);
-
-    switch(ch)
+    int queue[size], front = 0, rear = -1, ch;
+    do
     {
-            case 1: 
-            rear = enqueue(queue,rear,front);
+        printf("\n 1. Enqueue \n 2. DeQueue \n 3. Display \n Enter your choice : ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            rear = enqueue(queue, rear, front);
             break;
 
-            case 2: 
-            front = dequeue(queue,rear,front);
+        case 2:
+            front = dequeue(queue, rear, front);
             break;
 
-            case 3:
+        case 3:
 
-            display(queue,front,rear);
+            display(queue, front, rear);
             break;
 
-            default:
+        default:
             printf("Invalid choice\n");
+        }
 
-    }
+    } while (ch < 4);
 
-} while (ch<4);
-
-return 0;
+    return 0;
 }
 
-int enqueue(int queue[],int rear, int x)
+int enqueue(int queue[], int rear, int x)
 {
-    if(rear== size-1)
+    if (rear == size - 1)
     {
-        printf("Queue is Full Cannot Insert Ele");
+        printf("Queue is Full Cannot Insert Elemenet ");
+    }
+    else
+    {
+        printf("Enter value to insert : ");
+        scanf("%d", &x);
     }
 }
