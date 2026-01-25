19e8a92bdcb6293edb31dd1fb4151e174fd87a53
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-25 16:43:16 +0530

    Commit

--- QueueOps.c
+++ QueueOps.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define size 5
 
 int enqueue(int[], int, int);
 int dequeue(int[], int, int);
@@ -6,4 +7,20 @@ int display(int[], int, int);
 
 main()
 {
+
+int queue[size],front = 0,rear = -1 ,ch;
+do
+{
+    printf("\n 1. Enqueue \n 2. DeQueue \n 3. Display \n Enter your choice : ");
+    scanf("%d",&ch);
+
+    switch(ch)
+    {
+            case 1: 
+            rear = enqueue()
+    }
+
+} while (ch<4);
+
+
 }
