00b771f7857ab6c051da33e8a69fac6c058e6fc9
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-27 14:53:45 +0530

    Commit

--- QueuePush-pop.c
+++ QueuePush-pop.c
@@ -5,6 +5,7 @@ int enqueue(int[], int, int);
 
 main()
 {
-    int queue[SIZE], rear = -1, front = 0,item;
-    printf("Enter Element To ")
+    int queue[SIZE], rear = -1, front = 0, item;
+    printf("Enter Element To insert : ");
+    scanf("%d", &item);
 }
