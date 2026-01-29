d8acc7974fbb0c6bb919f9acc19c3061f19cdaff
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-29 20:05:20 +0530

    This is changed

--- circular_Queue.c
+++ circular_Queue.c
@@ -7,7 +7,30 @@ int Cdequeue(int[],int,int);
 main()
 {
     int  cq[max],rear=-1,front = -1,ch;
-     
+     do
+     {
+        printf("\n 1. insert queue \n 2. Delete Element \n 3. Display \n 4. peek \n Enter your choice \n ");
+        scanf("%d",&ch);
+
+        switch(ch)
+        {
+            case 1:
+            rear = Cenqueue(cq,front,rear);
+            if(front == -1 )
+            {
+                 front = 0;
+            }
+            break;
+
+            case 2:
+            rear = Cenqueue(cq,front,rear);
+            if(front == -1 )
+            {
+                 front = 0;
+            }
+            break;
+        }
+     }
     
 }
 
