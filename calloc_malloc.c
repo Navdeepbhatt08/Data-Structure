b77726999e11e9e548de809b23a810edcc62c840
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-30 08:13:02 +0530

    commmit

---
+++ calloc_malloc.c
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+
+main()
+{
+    int *p= NULL;
+    int i,n=0,s=0;
+    float avg = 0;
+    printf("Enter the number of elements ")
+    
+}
--- circular_Queue.c
+++ circular_Queue.c
@@ -1,36 +1,66 @@
-#include<stdio.h>
+#include <stdio.h>
 #define max 5
 
-int Cenqueue(int[],int,int);
-int Cdequeue(int[],int,int);
+int Cenqueue(int[], int, int);
+// int Cdequeue(int[],int,int);
+void display(int[], int, int);
 
 main()
 {
-    int  cq[max],rear=-1,front = -1,ch;
-     do
-     {
+    int cq[max], rear = -1, front = -1, ch;
+    do
+    {
         printf("\n 1. insert queue \n 2. Delete Element \n 3. Display \n 4. peek \n Enter your choice \n ");
-        scanf("%d",&ch);
+        scanf("%d", &ch);
 
-        switch(ch)
+        switch (ch)
         {
-            case 1:
-            rear = Cenqueue(cq,front,rear);
-            if(front == -1 )
+        case 1:
+            rear = Cenqueue(cq, front, rear);
+            if (front == -1)
             {
-                 front = 0;
+                front = 0;
             }
             break;
 
-            case 2:
-            rear = Cenqueue(cq,front,rear);
-            if(front == -1 )
-            {
-                 front = 0;
-            }
+        case 2:
+            display(cq, front, rear);
             break;
         }
-     }
-    
+    } while (ch != 3);
 }
 
+int Cenqueue(int cq[], int rear, int front)
+{
+    int x;
+    if ((rear + 1) % max == front)
+    {
+        printf("Queue is Full, Cannot insert ");
+    }
+    else
+    {
+        printf("Enter Element to insert : ");
+        scanf("%d", &x);
+
+        rear = (rear + 1) % max;
+        cq[rear] = x;
+        printf("Element inserted %d ", x);
+    }
+    return (rear);
+}
+
+void display(int cq[], int front, int rear)
+{
+    if (front == -1 && rear == -1)
+    {
+        printf("Queue Is Empty");
+    }
+    else
+    {
+        while (front != rear)
+        {
+            printf("%d ", cq[front]);
+            front = (front + 1) % max;
+        }
+    }
+}
