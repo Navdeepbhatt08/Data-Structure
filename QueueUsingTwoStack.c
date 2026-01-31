abb800082c1fa444cc2e5f6dd4e1d731d00ad7bd
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-01-31 11:29:52 +0530

    Git Commit

--- QueueUsingTwoStack.c
+++ QueueUsingTwoStack.c
@@ -1,3 +1,20 @@
 #include<stdio.h>
 #define max 5
 
+struct queue
+{
+    int stk1[max];
+    int stk2[max];
+    int top1;
+    int top2;
+};
+
+
+struct  queue insert(struct queue
+{
+    /* data */
+};
+)
+{
+    /* data */
+};
