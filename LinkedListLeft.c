ca9265f3fe827c2f4c6e669c42d5e404e9309cc4
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-02-05 17:35:18 +0530

    Commit

--- LinkedListLeft.c
+++ LinkedListLeft.c
@@ -5,33 +5,5 @@ typedef struct Node {
     struct Node* next;
 } sl;
 
-sl* createNode(int value) {
-    sl* newNode = (sl*)malloc(sizeof(sl));
-    newNode->data = value;
-    newNode->next = NULL;
-    return newNode;
-}
-sl* insertAtBeginning(sl* head, int value) {
-    sl* newNode = createNode(value);
-    newNode->next = head;
-    return newNode;
-}
-void displayList(sl* head) {
-    sl* current = head;
-    while (current != NULL) {
-        printf("%d -> ", current->data);
-        current = current->next;
-    }
-    printf("NULL\n");
-}
-int main() {
-    sl* head = NULL;
-
-    head = insertAtBeginning(head, 10);
-    head = insertAtBeginning(head, 20);
-    head = insertAtBeginning(head, 30);
-
-    displayList(head);
-
-    return 0;
-}
+main()
+{}
