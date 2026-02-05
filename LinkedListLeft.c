0ee58dd730c564d88f94ff9e561536cacef4cf76
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-02-05 17:39:14 +0530

    commit

--- LinkedListLeft.c
+++ LinkedListLeft.c
@@ -7,24 +7,5 @@ typedef struct Node {
 
 main()
 {
-    int n, i;
-    sl *head = NULL, *newNode, *temp;       
-
-    printf("Enter the number of nodes: ");
-    scanf("%d", &n);
-    for(i = 0; i < n; i++)
-    {
-        newNode = (sl*)malloc(sizeof(sl));
-        printf("Enter data for node %d: ", i + 1);
-        scanf("%d", &newNode->data);
-        newNode->next = head;
-        head = newNode;
-    }
-    printf("The linked list is: ");
-    temp = head;
-    while(temp != NULL)
-    {
-        printf("%d -> ", temp->data);
-        temp = temp->next;
-    }
+    int 
 }
