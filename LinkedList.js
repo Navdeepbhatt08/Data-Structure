d25462a31bd4c26681e4eaa38d924a83b2bb6070
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-02-07 20:26:34 +0530

    commit

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,22 @@
-function node()
+class Node
 {
+    constructor(data,next=null)
+    {
+        this.data = data;
+        this.next = next
+    }
+}
+
+class linkedList
+{
+    constructor()
+    {
+        this.head = null;
+        this.size = 0;
+    }
+
+    //Insert  First Node
+    
+
 
 }
