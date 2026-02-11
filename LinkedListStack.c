909c5614f708716b74c0f38bea3054292deb467c
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-02-11 17:27:21 +0530

    git push commit

--- LinkedListStack.c
+++ LinkedListStack.c
@@ -1,20 +1,107 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct stack
-{
+typedef struct Node {
     int data;
-    struct stack* next;
-} stack ;
+    struct Node* next;
+} Node;
 
-stack * push(stack*);
-stack * pop(stack*);
-void display(stack*);
+// PUSH (Call by Value)
+Node* push(Node* top, int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
 
-main()
-{
+    if (newNode == NULL) {
+        printf("Stack Overflow\n");
+        return top;
+    }
 
-    int top,ch;
-    
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+
+    printf("%d pushed into stack\n", value);
+    return top;
 }
 
+// POP (Call by Value)
+Node* pop(Node* top) {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        return top;
+    }
+
+    Node* temp = top;
+    printf("%d popped from stack\n", top->data);
+
+    top = top->next;
+    free(temp);
+
+    return top;
+}
+
+void peek(Node* top) {
+    if (top == NULL)
+        printf("Stack is empty\n");
+    else
+        printf("Top element is: %d\n", top->data);
+}
+
+void display(Node* top) {
+    Node* temp = top;
+
+    if (temp == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    printf("Stack elements:\n");
+    while (temp != NULL) {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    Node* top = NULL;
+    int choice, value;
+
+    while (1) {
+        printf("\n----- STACK MENU -----\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Peek\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                top = push(top, value);
+                break;
+
+            case 2:
+                top = pop(top);
+                break;
+
+            case 3:
+                peek(top);
+                break;
+
+            case 4:
+                display(top);
+                break;
+
+            case 5:
+                printf("Exiting program...\n");
+                exit(0);
+
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+
+    return 0;
+}
