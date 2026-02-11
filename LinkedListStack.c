c478fb2ac6cf89fc83bd08377d5b87bc87f3a464
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-02-11 17:03:17 +0530

    GIt Commit

---
+++ LinkedListStack.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct stack
+{
+    int data;
+    struct stack* next;
+} stack ;
+
+stack * push(stack*);
+stack * pop(stack*);
+void display(stack*);
+
