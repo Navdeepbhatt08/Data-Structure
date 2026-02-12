9398e4d8336c269a0cfc26d8ea36ca38a19aee0f
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-02-12 21:46:39 +0530

    COMMIT

---
+++ Stack_Array.c
@@ -0,0 +1,15 @@
+#define max 10
+#include<stdio.h>
+
+int push(int ,int[]);
+int pop(int ,int[]);
+void display(int ,int[]);
+
+main()
+{
+ 
+    int stack[max],top= -1,ch;
+    printf("1.Push \n2.Pop \n3.Display \nEnter your choice : ");
+    scanf("%d",&ch);
+    
+}
