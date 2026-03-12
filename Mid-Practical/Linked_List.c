5d48100058b13f958ec420b8fffa3141664570c8
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-12 18:41:16 +0530

    Commmit Saved

--- Mid-Practical/Linked_List.c
+++ Mid-Practical/Linked_List.c
@@ -9,5 +9,20 @@ typedef struct slist
 
 slist *insert(slist *ptr)
 {
+    int x ;
+    slist *p = NULL;
     
+    p = (slist*)malloc(sizeof(slist));
+    if(p==NULL)
+    {
+        printf("MEmory Allocation failed ");
+        return ptr;
+    }
+    printf("Enter  Data : " );
+    scanf("%d",&x);
+    p ->data = x;
+    p->next = ptr ;
+    ptr = p;
+
+    return ptr;
 }
