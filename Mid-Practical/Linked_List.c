97ff8225a200d192672603eca40b85ae38949046
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-13 21:20:12 +0530

    Commit Saved

--- Mid-Practical/Linked_List.c
+++ Mid-Practical/Linked_List.c
@@ -23,6 +23,91 @@ slist *insert(slist *ptr)
     p ->data = x;
     p->next = ptr ;
     ptr = p;
+    return ptr;
+}
+
+slist *insertEnd(slist *ptr)
+{
+    int x;
+    slist *p = NULL, *temp = NULL;
+
+    p = (slist*)malloc(sizeof(slist));
+    if(p == NULL)
+    {
+        printf("Memory Allocation failed");
+        return ptr;
+    }
+
+    printf("Enter Data: ");
+    scanf("%d", &x);
+
+    p->data = x;
+    p->next = NULL;
+
+    if(ptr == NULL)   // If list is empty
+    {
+        ptr = p;
+    }
+    else
+    {
+        temp = ptr;
+        while(temp->next != NULL)   // Traverse to last node
+        {
+            temp = temp->next;
+        }
+        temp->next = p;   // Insert at end
+    }
 
     return ptr;
+}
+
+
+void display(slist *node)
+{  
+    if(node == NULL)
+    {
+        printf("List is Empty \n");
+        return ;
+    }
+    while(node !=NULL)
+    {
+        printf("%d->",node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+main()
+{
+    int ch;
+    slist *head = NULL;
+    do
+    {
+         printf("\nEnter choice : ");
+        printf("\n1. Insert At Left");
+        printf("\n2. Insert At Right");
+        printf("\n3. Display");
+        printf("\n4. Exit");
+        printf("\nChoice: ");
+        scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+    head = insert(head);
+    break;
+    
+    case 2:
+    head = insertEnd(head);
+    break;
+    case 3:
+    display(head);
+    break;
+
+    default:
+        break;
+    }
+
+
+    } while (ch<4);
+
 }
