d0a590abefc9648ec1f6c90d9c011b02b537d834
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-15 10:22:41 +0530

    SAVED

--- Mid-Practical/MyLinkedList.c
+++ Mid-Practical/MyLinkedList.c
@@ -20,3 +20,31 @@ slist *insertLeft(slist *ptr)
     ptr = p;
     return ptr;
 }
+
+slist *insertRight(slist *ptr)
+{
+    int x;
+    slist *p = NULL, *temp = NULL;
+    p = (slist *)malloc(sizeof(slist));
+
+    printf("Enter Data To Insert : ");
+    scanf("%d", &x);
+    p->data = x;
+    p->next = NULL;
+
+    if (ptr == NULL)
+    {
+        ptr = p;
+    }
+    else
+    {
+        temp = ptr;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = p;
+    }
+
+    return ptr;
+}
