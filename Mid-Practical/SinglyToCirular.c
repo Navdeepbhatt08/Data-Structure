36d327c808501c3ef8cf05c9e1ddb36a7a856d17
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-16 17:57:03 +0530

    Circular

---
+++ Mid-Practical/SinglyToCirular.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct slist
+{
+    int data;
+    struct slist *next;
+} node;
+
+node *insert(node *ptr)
+{
+    int x;
+    node *p = NULL;
+    p = (node *)malloc(sizeof(node));
+
+    printf("Enter x : ");
+    scanf("%d", &x);
+    p->data = x;
+    p->next = ptr;
+    ptr = p;
+    return ptr;
+}
+
+void display(node *ptr)
+{
+    if (ptr == NULL)
+    {
+        printf("List is Empty \n");
+        return;
+    }
+    while (ptr != NULL)
+    {
+        printf("%d->", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+
+node *convertCircular(node *head)
+{
+    node *ptr = head;
+
+    if (head == NULL)
+    {
+        printf("List is Empty\n");
+        return head;
+    }
+
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = head;
+    printf("back to first node\n");
+    return head;
+}
+
+main()
+{
+    int ch;
+    node *head = NULL;
+    do
+    {
+        printf("\nEnter choice : ");
+        printf("\n1. Insert At Left");
+        printf("\n2. Display");
+        printf("\n3. Convert into Circular");
+        printf("\nChoice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            head = insert(head);
+            break;
+
+        case 2:
+            display(head);
+            break;
+        case 3:
+            head = convertCircular(head, head);
+            break;
+
+        default:
+            break;
+        }
+
+    } while (ch < 100);
+}
