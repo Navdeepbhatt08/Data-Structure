9011b2695efe4eaa4a0cc56c2899f2392e0076ae
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-22 20:28:44 +0530

    Commit Saved

--- CountFreq.js
+++ CountFreq.js
@@ -2,18 +2,14 @@ function countFrequency(arr) {
     let n = arr.length;
 
     for (let i = 0; i < n; i++) {
-        // Skip already counted elements
         if (arr[i] === null) continue;
-
         let count = 1;
-
         for (let j = i + 1; j < n; j++) {
             if (arr[i] === arr[j]) {
                 count++;
                 arr[j] = null; 
             }
         }
-
         console.log(arr[i] + " occurs " + count + " times");
     }
 }
