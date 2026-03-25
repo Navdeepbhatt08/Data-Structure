1ac9602d438f9ddadf7afc2cf2cca6d9dbb4bd94
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-25 19:13:46 +0530

    BST Created

--- TreeOperations.c
+++ TreeOperations.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct BinarySearchTree
 {
@@ -8,11 +8,104 @@ typedef struct BinarySearchTree
     struct BinarySearchTree *right;
 } bst;
 
+// Insert node
 void create(bst **rt, int v)
 {
-    
+    if (*rt == NULL)
+    {
+        bst *p = (bst *)malloc(sizeof(bst));
+        p->data = v;
+        p->left = NULL;
+        p->right = NULL;
+        *rt = p;
+    }
+    else if ((*rt)->data > v)
+    {
+        create(&((*rt)->left), v);
+    }
+    else if ((*rt)->data < v)
+    {
+        create(&((*rt)->right), v);
+    }
+    else
+    {
+        printf("Duplicate data not allowed\n");
+    }
 }
 
-main()
+// Preorder
+void preorder(bst *rt)
 {
+    if (rt != NULL)
+    {
+        printf("%d ", rt->data);
+        preorder(rt->left);
+        preorder(rt->right);
+    }
+}
+
+// Inorder
+void inorder(bst *rt)
+{
+    if (rt != NULL)
+    {
+        inorder(rt->left);
+        printf("%d ", rt->data);
+        inorder(rt->right);
+    }
+}
+
+// Postorder
+void postorder(bst *rt)
+{
+    if (rt != NULL)
+    {
+        postorder(rt->left);
+        postorder(rt->right);
+        printf("%d ", rt->data);
+    }
+}
+
+int main()
+{
+    bst *root = NULL;
+    int ch, v;
+
+    while (1)
+    {
+        printf("\n1. Insert\n2. Preorder\n3. Inorder\n4. Postorder\n5. Exit\nEnter choice: ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            printf("Enter value: ");
+            scanf("%d", &v);
+            create(&root, v);
+            break;
+
+        case 2:
+            printf("Preorder: ");
+            preorder(root);
+            break;
+
+        case 3:
+            printf("Inorder: ");
+            inorder(root);
+            break;
+
+        case 4:
+            printf("Postorder: ");
+            postorder(root);
+            break;
+
+        case 5:
+            exit(0);
+
+        default:
+            printf("Invalid choice\n");
+        }
+    }
+
+    return 0;
 }
