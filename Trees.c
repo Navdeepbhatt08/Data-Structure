d5151871be7dab6bd5c9c9f6ffc9df375793b04b
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-27 22:15:30 +0530

    Git Commit Saved

--- Trees.c
+++ Trees.c
@@ -1,20 +1,68 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct binarySearchTree{
+typedef struct binarySearchTree
+{
     struct binarySearchTree *left;
     int data;
     struct binarySearchTree *right;
 } bst;
 
-void create(bst **root,int val)
+void create(bst **root, int val)
 {
+    (*root) = malloc(sizeof(bst));
+    if ((*root) = NULL)
+    {
+        (*root)->data = val;
+        (*root)->left = NULL;
+        (*root)->right = NULL;
+        return;
+    }
+    else if (((*root)->data) > val)
+    {
+        create(((*root)->left), val);
+    }
+    else if (((*root)->data) < val)
+    {
+        create(((*root)->right), val);
+    }
+    else
+    {
+        return;
+    }
+}
 
+void preorder(bst *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
 }
 
 main()
 {
     bst *root = NULL;
-    int ch,val;
+    int ch, val;
 
+    printf("1. Create \n2. Display \nEnter : ");
+    scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+    printf("Enter Data : ");
+        scanf("%d", &val);
+        create(&(*root), val);
+        break;
+    case 2:
+        preorder(root);
+        break;
+
+    default:
+        break;
+    }
+    while (ch < 4)
+        ;
 }
