df59b38f27634786e950297a46d04e8da87e1b7a
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-27 17:44:08 +0530

    Commit Saved

--- Trees.c
+++ Trees.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+typedef struct binarySearchTree{
+    struct binarySearchTree *left;
+    int data;
+    struct binarySearchTree *right;
+} bst;
+
+void create(bst **root,int val)
+{
+
+}
+
+main()
+{
+    bst *root = NULL;
+    int ch,val;
+
+}
