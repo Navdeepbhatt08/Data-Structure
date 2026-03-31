18e679814577cd818df6685bc56ab264104267f6
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2026-03-31 19:08:17 +0530

    Commiit

--- TreeOperations.c
+++ TreeOperations.c
@@ -1,63 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node
+typedef struct TreeOperations
 {
+    struct TreeOperations *left, *right;
     int data;
-    struct Node *left;
-    struct Node *right;
-};
+} bst;
 
-struct Node *createNode(int data)
+void create(bst **root, int v)
 {
-    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
-    node->data = data;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-}
-
-struct Node *insert(struct Node *root, int data)
-{
-    if (root == NULL)
-    {
-        return createNode(data);
-    }
-
-    if (data < root->data)
-    {
-        root->left = insert(root->left, data);
-    }
-    else
-    {
-        root->right = insert(root->right, data);
-    }
-
-    return root;
-}
-
-void inorder(struct Node *root)
-{
-    if (root == NULL)
-        return;
-    inorder(root->left);
-    printf("%d ", root->data);
-    inorder(root->right);
-}
-
-int main()
-{
-    struct Node *root = NULL;
-
-    root = insert(root, 50);
-    insert(root, 30);
-    insert(root, 70);
-    insert(root, 20);
-    insert(root, 40);
-
-    printf("Inorder: ");
-    inorder(root);
-    printf("\n");
-
-    return 0;
 }
